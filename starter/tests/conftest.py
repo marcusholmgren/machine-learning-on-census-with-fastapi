f052e43ff40d7892f43a6e4ccca5d9d4c3605e1b
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-11-07 13:04:47 +0100

    feat: FastAPI with prediction endpoint

--- starter/tests/conftest.py
+++ starter/tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 import pandas as pd
-
+from starlette.testclient import TestClient
 from starter.starter.ml.data import process_data
+from starter.main import app
 
 
 @pytest.fixture(scope="module")
@@ -22,3 +23,13 @@ def get_process_data(clean_data_df):
                                      lb=None
                                      )
     return X, y, encoder, lb
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    # set up
+    with TestClient(app) as test_client:
+        # testing
+        yield test_client
+
+    # tear down
